7c5d53b90dc6c084ab739d49bfa6cb420429e5b3
yatinkumar01 <yatins9152015@gmail.com>
2023-01-19 00:25:55 +0530

    submit

---
+++ OnlineCabBookingSystem/src/test/java/com/example/onlinecabbookingsystem/OnlineCabBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlinecabbookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCabBookingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
