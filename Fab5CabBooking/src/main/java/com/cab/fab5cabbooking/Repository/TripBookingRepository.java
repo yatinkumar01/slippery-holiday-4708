48526039d726aca6ed2832763753dbe2789f18c7
jkka777 <abhiram.kishore@gmail.com>
2023-01-19 22:51:23 +0530

    added trip booking service, repository, exception by kishore fw19_0718

---
+++ Fab5CabBooking/src/main/java/com/cab/fab5cabbooking/Repository/TripBookingRepository.java
@@ -0,0 +1,6 @@
+package com.cab.fab5cabbooking.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TripBookingRepository extends JpaRepository<TripBookingRepository, Integer> {
+}
