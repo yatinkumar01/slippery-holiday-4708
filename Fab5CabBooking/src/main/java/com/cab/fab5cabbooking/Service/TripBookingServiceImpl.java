fa4c2b28855e5cfbf7349fa71f5a048a8e92a0e1
jkka777 <abhiram.kishore@gmail.com>
2023-01-19 23:03:00 +0530

    added trip booking service, exception and repo by Kishore fw19_0718

--- Fab5CabBooking/src/main/java/com/cab/fab5cabbooking/Service/TripBookingServiceImpl.java
+++ Fab5CabBooking/src/main/java/com/cab/fab5cabbooking/Service/TripBookingServiceImpl.java
@@ -8,11 +8,13 @@ import com.cab.fab5cabbooking.Model.TripBooking;
 import com.cab.fab5cabbooking.Repository.CustomerRepository;
 import com.cab.fab5cabbooking.Repository.TripBookingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class TripBookingServiceImpl implements TripBookingService {
 
     @Autowired
