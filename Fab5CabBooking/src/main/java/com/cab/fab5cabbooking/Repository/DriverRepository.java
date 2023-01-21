1688e2d5d2c860a83eb1b0d4325bed4ea6d7bf8c
yatinkumar01 <yatins9152015@gmail.com>
2023-01-21 15:09:26 +0530

    submit

---
+++ Fab5CabBooking/src/main/java/com/cab/fab5cabbooking/Repository/DriverRepository.java
@@ -0,0 +1,8 @@
+package com.cab.fab5cabbooking.Repository;
+
+import com.cab.fab5cabbooking.Model.Driver;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DriverRepository extends JpaRepository<Driver, Integer> {
+
+}
